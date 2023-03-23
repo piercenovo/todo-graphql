a174a1938043025d2a0a4e1bf3379dd37f0aecff
piercenovo <piercenovoah@gmail.com>
2023-03-23 18:30:29 -0500

    feat(api): Add Create Project with GraphQL

--- api/src/graphql/resolvers.ts
+++ api/src/graphql/resolvers.ts
@@ -1,5 +1,16 @@
+import { type Project as ProjectType } from 'types'
+import Project from '../models/project.model'
+
 export const resolvers = {
   Query: {
-    hello: () => 'Hello world'
+    hello: () => 'Hello world',
+    projects: async () => await Project.find()
+  },
+  Mutation: {
+    createProject: async (_: any, { name, description }: ProjectType) => {
+      const project = new Project({ name, description })
+      const savedProject = await project.save()
+      return savedProject
+    }
   }
 }
--- api/src/graphql/typeDefs.ts
+++ api/src/graphql/typeDefs.ts
@@ -3,4 +3,18 @@ import { gql } from 'graphql-tag'
 export const typeDefs = gql`
   type Query {
     hello: String
-  }`
+    projects: [Project]
+  }
+
+  type Mutation {
+    createProject(name: String, description: String): Project
+  }  
+
+  type Project {
+    _id: ID
+    name: String
+    description: String
+    createdAt: String
+    updatedAt: String
+  }
+`
